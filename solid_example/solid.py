8e30eb3ddcc7a339ab56bea78ef641222ba2e874
dofo <aserranom29@gmail.com>
2023-05-09 19:03:47 -0600

    fixing L

--- solid_example/solid.py
+++ solid_example/solid.py
@@ -21,21 +21,37 @@ class Order:
 
 class PaymentProcessor(ABC):
     @abstractmethod
-    def pay(self, order, security_code):
+    def pay(self, order):
         pass
 
 
 class DebitPaymentProcessor(PaymentProcessor):
-    def pay(self, order, security_code):
+    def __init__(self, security_code):
+        self.security_code = security_code
+
+    def pay(self, order):
         print("Processing debit payment type")
-        print(f"Verifying security code: {security_code}")
+        print(f"Verifying security code: {self.security_code}")
         order.status = "paid"
 
 
 class CreditPaymentProcessor(PaymentProcessor):
-    def pay(self, order, security_code):
+    def __init__(self, security_code):
+        self.security_code = security_code
+
+    def pay(self, order):
         print("Processing credit payment type")
-        print(f"Verifying security code: {security_code}")
+        print(f"Verifying security code: {self.security_code}")
+        order.status = "paid"
+
+
+class PaypalPaymentProcessor(PaymentProcessor):
+    def __init__(self, email_address):
+        self.email_address = email_address
+
+    def pay(self, order):
+        print("Processing paypal payment type")
+        print(f"Verifying email address: {self.email_address}")
         order.status = "paid"
 
 
@@ -46,5 +62,5 @@ order1.add_item("USB cable", 2, 5)
 
 print(f"Total order price: {order1.total_price()}")
 
-processor = DebitPaymentProcessor()
-processor.pay(order1, "0123456ABC")
+processor = DebitPaymentProcessor("0345678")
+processor.pay(order1)
