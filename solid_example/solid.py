a75003a4be7094b2107dee82be8351f0e04ffd80
dofo <aserranom29@gmail.com>
2023-05-09 19:22:32 -0600

    adding 2FA via sms

--- solid_example/solid.py
+++ solid_example/solid.py
@@ -24,15 +24,26 @@ class PaymentProcessor(ABC):
     def pay(self, order):
         pass
 
+    @abstractmethod
+    def auth_sms(self,  code):
+        pass
+
 
 class DebitPaymentProcessor(PaymentProcessor):
     def __init__(self, security_code):
         self.security_code = security_code
+        self.verified = False
 
     def pay(self, order):
+        if not self.verified:
+            raise Exception("Not authorized")
         print("Processing debit payment type")
         print(f"Verifying security code: {self.security_code}")
         order.status = "paid"
+
+    def auth_sms(self,  code):
+        print(f"Verifying SMS code {code}")
+        self.verified = True
 
 
 class CreditPaymentProcessor(PaymentProcessor):
@@ -44,15 +55,25 @@ class CreditPaymentProcessor(PaymentProcessor):
         print(f"Verifying security code: {self.security_code}")
         order.status = "paid"
 
+    def auth_sms(self,  code):
+        raise Exception("Credit card payments don't support SMS code authorization")
+
 
 class PaypalPaymentProcessor(PaymentProcessor):
     def __init__(self, email_address):
         self.email_address = email_address
+        self.verified = False
 
     def pay(self, order):
+        if not self.verified:
+            raise Exception("Not authorized")
         print("Processing paypal payment type")
         print(f"Verifying email address: {self.email_address}")
         order.status = "paid"
+
+    def auth_sms(self,  code):
+        print(f"Verifying SMS code {code}")
+        self.verified = True
 
 
 order1 = Order()
@@ -63,4 +84,5 @@ order1.add_item("USB cable", 2, 5)
 print(f"Total order price: {order1.total_price()}")
 
 processor = PaypalPaymentProcessor("adolfo@intel.com")
+processor.auth_sms("0456")
 processor.pay(order1)
