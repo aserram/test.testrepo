0e6084faf621c55825e263cc1874cfa1bd86ffee
dofo <aserranom29@gmail.com>
2025-02-21 14:45:31 -0600

    Adding file for new leetcode problem

---
+++ leetcode/Median2SortedArrays/main.py
@@ -0,0 +1,11 @@
+from typing import Optional, List, TypeVar
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        pass
+
+def main():
+    pass
+
+if __name__ == "main":
+    main()
