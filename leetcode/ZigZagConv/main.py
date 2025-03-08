5c1622c2f40a66319d588481029fe4a724b728e4
dofo <aserranom29@gmail.com>
2025-03-07 18:07:26 -0600

    ZZ convertion solution
    solution from deleted_user Aug28, 2020

---
+++ leetcode/ZigZagConv/main.py
@@ -0,0 +1,27 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        main_column = True
+        rowidx = 0
+        zz_covertion = [""] * numRows
+        for char in s:
+            zz_covertion[rowidx] += char
+            if main_column:
+                rowidx += 1
+            else:
+                rowidx -= 1
+
+            if rowidx == numRows - 1:
+                main_column = False
+            elif rowidx == 0:
+                main_column = True
+        return "".join(zz_covertion)
+
+
+def main():
+    target = Solution()
+    result_str = target.convert("CULOCONCACA", 3)
+    print(result_str)
+
+
+if __name__ == "__main__":
+    main()
