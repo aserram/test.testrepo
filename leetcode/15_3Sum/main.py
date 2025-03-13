4de84d61168b62007bf910c47d65fc1c9e514612
Dofo <aserranom29@gmail.com>
2025-03-12 23:29:21 -0600

    more fixes

--- leetcode/15_3Sum/main.py
+++ leetcode/15_3Sum/main.py
@@ -5,7 +5,6 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         results = []
         numMap = {}
-        nums.sort()
 
         for idx, num in enumerate(nums):
             numMap[num] = idx
@@ -14,9 +13,9 @@ class Solution:
             if target_idx > 0 and nums[target_idx] == nums[target_idx - 1]:
                 continue
             for num_idx in range(target_idx + 1, len(nums)):
-                complement = (-1 * nums[target_idx]) - nums[num_idx]
+                complement = -1 * (nums[target_idx] + nums[num_idx])
                 if complement in numMap and numMap[complement] not in [target_idx, num_idx]:
-                    triplet = [nums[target_idx], complement, nums[num_idx]]
+                    triplet = sorted([nums[target_idx], complement, nums[num_idx]])
                     if triplet not in results:
                         results.append(triplet)
                 numMap[nums[num_idx]] = num_idx
@@ -25,7 +24,7 @@ class Solution:
 
 def main():
     target = Solution()
-    result = target.threeSum([0, 0, 0])
+    result = target.threeSum([-1, 0, 1, 2, -1, -4])
     print(result)
 
 
