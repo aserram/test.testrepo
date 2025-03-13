f7febd1d27cc4d0dbd894e1334ab4a07b26d34b9
Dofo <aserranom29@gmail.com>
2025-03-12 22:45:49 -0600

    3sum solution changes

--- leetcode/15_3Sum/main.py
+++ leetcode/15_3Sum/main.py
@@ -7,9 +7,6 @@ class Solution:
         nums.sort()
         numMap = {}
 
-        for idx, num in enumerate(nums):
-            numMap[num] = idx
-
         for target_idx in range(len(nums) - 2):
             if target_idx > 0 and nums[target_idx] == nums[target_idx - 1]:
                 continue
@@ -17,8 +14,9 @@ class Solution:
                 complement = (-1 * nums[target_idx]) - nums[num_idx]
                 if complement in numMap:
                     triplet = [nums[target_idx], complement, nums[num_idx]]
-                    if triplet not in results:
+                    if triplet not in results and numMap[complement] > target_idx and numMap[complement] < num_idx:
                         results.append(triplet)
+                numMap[nums[num_idx]] = num_idx
         return results
 
 
