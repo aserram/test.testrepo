ab005f0b9e82011789064810b1ca4f4f45d2a5c3
Dofo <aserranom29@gmail.com>
2025-03-13 15:55:16 -0600

    hash implementation O(n^2)

--- leetcode/15_3Sum/main_hashmap.py
+++ leetcode/15_3Sum/main_hashmap.py
@@ -12,11 +12,10 @@ class Solution:
         for target_idx in range(len(nums) - 2):
             for num_idx in range(target_idx + 1, len(nums)):
                 complement = -1 * (nums[target_idx] + nums[num_idx])
-                if complement in numMap and numMap[complement] not in [target_idx, num_idx]:
+                if complement in numMap and numMap[complement] != num_idx and numMap[complement] > target_idx:
                     triplet = sorted([nums[target_idx], complement, nums[num_idx]])
                     if triplet not in results:
                         results.append(triplet)
-                numMap[nums[num_idx]] = num_idx
         return results
 
 
