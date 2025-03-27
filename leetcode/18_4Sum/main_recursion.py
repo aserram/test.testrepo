488e218b0e9af8fd40a0dcddd60372ff349966b8
Dofo <aserranom29@gmail.com>
2025-03-27 16:05:34 -0600

    recursive solution kSum

--- leetcode/18_4Sum/main_recursion.py
+++ leetcode/18_4Sum/main_recursion.py
@@ -2,32 +2,39 @@ from typing import List
 
 
 class Solution:
+    def __init__(self):
+        self.results = []
+        self.quad = []
+
+    def two_sum(self, nums, left, right, target):
+        while left < right:
+            two_sum = nums[left] + nums[right]
+            if two_sum == target:
+                self.results.append(self.quad + [nums[left], nums[right]])
+                left += 1
+                while nums[left - 1] == nums[left] and left < right:
+                    left += 1
+            elif two_sum < target:
+                left += 1
+            else:
+                right -= 1
+
+    def k_sum(self, k, nums, start, target):
+        if k == 2:
+            self.two_sum(nums, start, len(nums) - 1, target)
+        else:
+            for idx in range(start, len(nums)):
+                if idx > start and nums[idx] == nums[idx - 1]:
+                    continue
+                self.quad.append(nums[idx])
+                self.k_sum(k - 1, nums, idx + 1, target - nums[idx])
+                self.quad.pop()
+
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
+        self.k_sum(4, nums, 0, target)
 
-        results = []
-
-        for idx_four, num_four in enumerate(nums):
-            if idx_four > 0 and nums[idx_four - 1] == num_four:
-                continue
-
-            for idx_three, num_three in enumerate(nums[idx_four + 1 : len(nums) - 1], start=idx_four + 1):
-                if idx_three > idx_four + 1 and nums[idx_three - 1] == num_three:
-                    continue
-
-                left, right = idx_three + 1, len(nums) - 1
-                while left < right:
-                    four_sum = num_four + num_three + nums[left] + nums[right]
-                    if four_sum == target:
-                        results.append([num_four, num_three, nums[left], nums[right]])
-                        left += 1
-                        while nums[left - 1] == nums[left] and left < right:
-                            left += 1
-                    elif four_sum < target:
-                        left += 1
-                    else:
-                        right -= 1
-        return results
+        return self.results
 
 
 def main():
