de4ebf7c3aa3f2bc07a6ee7e34ba9f36b40044e0
Dofo <aserranom29@gmail.com>
2025-04-02 23:00:07 -0600

    solution from youtube

--- leetcode/19_remove_nth_node/main.py
+++ leetcode/19_remove_nth_node/main.py
@@ -27,17 +27,30 @@ class LinkedList:
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        pass
+        res = ListNode(0, head)
+        dummy = res
+
+        for _ in range(n):
+            head = head.next
+
+        while head:
+            head = head.next
+            dummy = dummy.next
+
+        dummy.next = dummy.next.next
+
+        return res.next
 
 
 def main():
-    target = Solution()
     node5 = ListNode(5)
     node4 = ListNode(4, node5)
     node3 = ListNode(3, node4)
     node2 = ListNode(2, node3)
     node1 = ListNode(1, node2)
-    linkedlist = LinkedList(node1)
+    target = Solution()
+    result = LinkedList(target.removeNthFromEnd(node1, 2))
+    print(result)
 
 
 if __name__ == "__main__":
