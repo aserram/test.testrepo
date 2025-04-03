2b1bef11e37dc8c000ab15226c79973979487abc
Dofo <aserranom29@gmail.com>
2025-04-03 15:31:42 -0600

    solution leetc 20 valid parentheses

---
+++ leetcode/20_valid_parentheses/main.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bracket_mapping = {"(": ")", "[": "]", "{": "}"}
+        opening_stack = []
+
+        if len(s) % 2:
+            return False
+
+        for char in s:
+            if char in bracket_mapping.keys():
+                opening_stack.append(char)
+            elif char in bracket_mapping.values():
+                if not opening_stack or (opening_stack and bracket_mapping[opening_stack.pop()] != char):
+                    return False
+
+        return True if not opening_stack else False
+
+
+def main():
+    target = Solution()
+    result = target.isValid("))")
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
