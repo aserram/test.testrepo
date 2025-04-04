b5be5045685e3059bfeb6d65eb984d8528dc3df2
Dofo <aserranom29@gmail.com>
2025-04-03 20:48:28 -0600

    Solution refactored, edge cases work

--- leetcode/21_merge_2_sorted_lists/main.py
+++ leetcode/21_merge_2_sorted_lists/main.py
@@ -12,29 +12,45 @@ class ListNode:
     next: Node = None
 
 
+class LinkedList:
+    def __init__(self, head: ListNode = None):
+        self.head = head
+
+    def __str__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(str(node.val))
+            node = node.next
+        return f"[{",".join(nodes)}]"
+
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
         pointer = dummy
-        while list1 or list2:
+        while list1 and list2:
             if list1.val <= list2.val:
-                dummy.val = list1.val
-                dummy.next = ListNode(list2.val, ListNode())
+                dummy.next = list1
+                list1 = list1.next
             else:
-                dummy.val = list2.val
-                dummy.next = ListNode(list1.val, ListNode())
+                dummy.next = list2
+                list2 = list2.next
+            dummy = dummy.next
 
-            list1 = list1.next
-            list2 = list2.next
-            dummy = dummy.next.next
-        return pointer
+        if list1:
+            dummy.next = list1
+        else:
+            dummy.next = list2
+
+        return pointer.next
 
 
 def main():
     list1 = ListNode(1, ListNode(2, ListNode(4)))
     list2 = ListNode(1, ListNode(3, ListNode(4)))
     target = Solution()
-    target.mergeTwoLists(list1, list2)
+    print(LinkedList(target.mergeTwoLists(list1, list2)))
 
 
 if __name__ == "__main__":
