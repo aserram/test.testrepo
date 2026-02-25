eed73dc66274a518d8fc1774e6b3515aaa7baadd
Adolfo Serrano <aserranom29@gmail.com>
2026-02-24 20:53:07 -0600

    simplifying logic at a time cost
    time cost is negligible outside a competition

--- leetcode75/greatest_common_div_str_1071.py
+++ leetcode75/greatest_common_div_str_1071.py
@@ -1,27 +1,14 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if str1 == str2:
-            return str1
-
-        if len(str1) <= len(str2):
-            min_str = str1
-            max_str = str2
-        else:
-            min_str = str2
-            max_str = str1
-
-        for prefix in range(len(min_str), 0, -1):
-            if len(max_str) % prefix or len(min_str) % prefix:
-                continue
-
-            gcd_candidate = min_str[:prefix]
-            if all(gcd_candidate * (len(s) // prefix) == s for s in (min_str, max_str)):
+        for prefix in range(min(len(str1), len(str2)), 0, -1):
+            gcd_candidate = str1[:prefix]
+            if all(gcd_candidate * (len(s) // prefix) == s for s in (str1, str2)):
                 return gcd_candidate
         return ""
 
 
-# str1 = "ABAB"
-# str2 = "ABA"
+# str1 = "ABCABC"
+# str2 = "ABC"
 str1 = "AAAAAAAAA"
 str2 = "AAACCC"
 
